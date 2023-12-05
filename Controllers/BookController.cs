506e2a20d16b9b0faa210f1cae8ef941a9200b3b
Marin Barbaud <marin.barbaud@gmail.com>
2023-12-05 16:31:12 +0100

    feat: httpPost

--- Controllers/BookController.cs
+++ Controllers/BookController.cs
@@ -3,8 +3,6 @@ using Microsoft.EntityFrameworkCore;
 using newWebAPI.Models;
 namespace newWebAPI.Controllers;
 
-#nullable disable
-
 // Indique que cette classe est un controlleur d'API
 [ApiController]
 
@@ -20,10 +18,55 @@ public class Bookcontroller: ControllerBase
    }
 
    [HttpGet]
+
+   // Mauvaise Pratique
    public async Task<IEnumerable<Book>> Get()
    {
       return await _context.Books.ToListAsync();
    }
 
+   // GET: api/Book/[id]
+   [HttpGet("{id}", Name = nameof(GetBook))]
+   public async Task<ActionResult<Book>> GetBook(int id)
+   {
+      var book = await _context.Books.FindAsync(id);
+      if (book == null)
+      {
+         return NotFound();
+      }
+      return book;
+   }
 
+   // POST: api/Book
+   // BODY: Book (JSON)
+   [HttpPost]
+   [ProducesResponseType(201, Type = typeof(Book))]
+   [ProducesResponseType(400)]
+   public async Task<ActionResult<Book>> PostBook([FromBody] Book book)
+   {
+      // we check if the parameter is null
+      if (book == null)
+      {
+         return BadRequest();
+      }
+      // we check if the book already exists
+      Book? addedBook = await _context.Books.FirstOrDefaultAsync(b => b.Title == book.Title);
+      if (addedBook != null)
+      {
+         return BadRequest("Book already exists");
+      }
+      else
+      {
+         // we add the book to the database
+         await _context.Books.AddAsync(book);
+         await _context.SaveChangesAsync();
+
+         // we return the book
+         return CreatedAtRoute(
+            routeName: nameof(GetBook),
+            routeValues: new { id = book.Id },
+            value: book);
+
+      }
+   }
 }
